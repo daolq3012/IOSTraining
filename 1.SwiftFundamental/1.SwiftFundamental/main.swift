719e51e0e91360ed2cd0a575a8b2033252a75a38
DaoLQ <le.quang.dao@framgia.com>
2018-01-15 14:02:14 +0700

    lession 1

---
+++ 1.SwiftFundamental/1.SwiftFundamental/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  1.SwiftFundamental
+//
+//  Created by DaoLQ on 1/15/18.
+//  Copyright © 2018 DaoLQ. All rights reserved.
+//
+
+import Foundation
+
+let numberOfPlanets = 8
+let diameterOfEarth: Double = 24859.8244 // In miles, from pole to pole.
+
+print("Welcome to our solar system!")
+print("There are \(numberOfPlanets) plan")
+print("Your are currently on Earth, which has a circumference of \(diameterOfEarth) miles")
